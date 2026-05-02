b3d399c6fd3bd1f25c7b1fc7ab39820378bbd12a
Jin Young Kim <wlsdud66523@kakao.com>
2026-05-03 06:57:12 +0900

    VIEWの分離ーV2

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member newMember = new Member(username, age);
+        memberRepository.save(newMember);
+
+        // Model에 데이터 전달
+        request.setAttribute("member",newMember);
+
+        return new MyView("/WEB-INFO/views/save-result.jsp");
+    }
+}
