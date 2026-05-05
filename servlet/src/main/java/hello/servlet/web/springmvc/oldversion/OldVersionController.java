62b262cc608fde84f2d1090169df227d5241685e
Jin Young Kim <wlsdud66523@kakao.com>
2026-05-06 08:33:56 +0900

    Handler Mapping, Handler Adapter

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/oldversion/OldVersionController.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.oldversion;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") // 결국엔, 스프링 빈의 이름 지정이다. 스프링 빈의 이름이 url형식으로 돼 있는 것 뿐
+public class OldVersionController implements Controller {
+    @Override
+    public @Nullable ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldVersionController.handleRequest");
+        return null;
+    }
+}
